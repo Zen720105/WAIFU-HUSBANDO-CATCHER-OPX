8b47556974cee78973a02caf26325d3c9167b2c4
Dath <zenm9371@gmail.com>
2024-10-03 09:50:48 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -6,7 +6,7 @@ class Config(object):
     sudo_users = "7526369190", "6961287189", "7334126640", "5920352309", "7078181502", "1414467834"
     GROUP_ID = -1002119873436
     TOKEN = "7367511450:AAFb78v-duwLXS0IZraAU33CEPxdg5SVCs0"
-    mongo_url = "mongodb+srv://Epic2:w85NP8dEHmQxA5s7@cluster0.tttvsf9.mongodb.net/?retryWrites=true&w=majority"
+    mongo_url = "mongodb+srv://userbot:userbot@cluster0.iweqz.mongodb.net/test?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/ed23556d07d33db18402d.jpg", "https://telegra.ph//file/e64337bbc6cdac7e6b178.jpg"]
     SUPPORT_CHAT = "waifexanime"
     UPDATE_CHAT = "waifebot_supoort"
